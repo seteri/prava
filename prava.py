26eed68554ccf64565b06fd7dc4a4cc3ee810936
unknown <wpapuna5@gmail.com>
2024-03-25 15:32:04 +0400

    f

--- prava.py
+++ prava.py
@@ -1,17 +1,17 @@
 import requests
 import time
 
-
 def sheamowme():
     url = "https://api-my.sa.gov.ge/api/v1/DrivingLicensePracticalExams2/DrivingLicenseExamsDates2?CategoryCode=3&CenterId=10&fbclid=IwAR0wWqq6Og1z6oPMnZd2eJBvx1k6ZVYNmpJUyFAzNo4FJRYwyeQ1bHKGo4c"
     response = requests.get(url)
     final = response.json()
+    print(final)
 
     if len(final) != 0:
         msg = {"content": "გაიხსნა"}
-
-        webhook_url = "https://discord.com/api/webhooks/1131331517919019048/5BFy84rXTlasBNpqSFyQ3dtgsC5iTjj3ec60VxE4umyHSYGeEc4TnKyRnJPimzwUOKah"
+        webhook_url = "https://discord.com/api/webhooks/1196206540395401318/hi2tr95qxXHhpWPYCz0yF-NjqLwhQ9QuZUBIydbtczvFUWCU1ch1TwhMM9rEWtQ5ZACj"
         requests.post(webhook_url, json=msg)
+        print("გაიგზავნა")
 
 
 
